9544f1232ba796de366d4c513d9653444c9e7a54
Riaz <81258483+R14Z@users.noreply.github.com>
2021-09-08 23:23:19 +0100

    updated version
    fixed some bugs, also changed the strings to variables so that if u or I get an error it would be easy to solve it.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,48 +4,51 @@ player_1 = input("Player 1: Rock Paper Or Scissors? ").lower()
 
 player_2 = input("Player 2: Rock Paper Or Scissors? ").lower()
 
+# validation
+valid_options = ['rock','paper','scissors']
+if player_1 not in valid_options or player_1 not in valid_options:
+    print('Error')
+    exit()
 
+if player_1 == player_2:
+    print('Draw!')
+
+# constant
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
 # rock
-
-# both players
-if player_1 == "rock" and player_2 == "rock":
-    print("Draw!")
 
 
 # rock scissors
 
-if player_1 == "rock" and player_2 == "scissors":
+if player_1 == rock and player_2 == scissors:
     print("Player 1 has won!")
-elif player_2 == "rock" and player_1 == "scissors":
-        print("Player 2 has won!")
+elif player_2 == rock and player_1 == scissors:
+    print("Player 2 has won!")
 
 
 # rock paper
 
-if player_1 == "rock" and player_2 == "paper":
+if player_1 == rock and player_2 == paper:
     print("Player 2 has won!")
-elif player_1 == "paper" and player_2 =="rock":
+elif player_1 == "paper" and player_2 == rock:
     print('Player 1 has won!')
 
 
 # scissors 
 
-if player_1 == 'scissors' and player_2 == 'scissors':
+if player_1 == scissors and player_2 == scissors:
     print('Draw! ')
 
 # scissors paper
 
-if player_1 == "scissors" and player_2 =="paper":
+if player_1 == scissors and player_2 == paper:
     print('Player 1 has won! ')
-elif player_2 == 'scissors' and player_1 == 'paper':
+elif player_2 == scissors and player_1 == paper:
     print('Player 2 has won! ')
 
 
 
 # paper
 
-if player_1 == 'paper' and player_2 == 'paper':
-    print('Draw! ')
-    
-else:
-    print('Error')
